40f316730c58c6eec8d63c38bf28eefc2279e51e
Gustavo Varallo <gustavo.varallo@live.com>
2021-02-23 18:25:26 -0300

    feat: add last 24 and 48 hours buttons

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="main">
     <div class="columns is-centered">
+      <div class="columns">
+        <div class="notification is-primary">
+          <div class="buttons">
+            <b-button type="is-primary" inverted @click="lastTwentyFour"
+              >Last 24 Hours</b-button
+            >
+            <b-button type="is-primary" inverted @click="lastFortyEight"
+              >Last 48 Hours</b-button
+            >
+          </div>
+        </div>
+      </div>
       <b-field class="calendar" label="Select a date">
         <b-datepicker
           class="column is-primary"
@@ -20,7 +32,6 @@
         </b-datepicker>
       </b-field>
     </div>
-    {{ dates }}
   </div>
 </template>
 
@@ -45,9 +56,23 @@ export default {
   },
 
   methods: {
+    lastTwentyFour() {
+      let first = new Date()
+      first.setDate(first.getDate() - 1)
+      const second = new Date()
+      Vue.set(this.dates, 0, new Date(first))
+      Vue.set(this.dates, 1, new Date(second))
+    },
+    lastFortyEight() {
+      let first = new Date()
+      first.setDate(first.getDate() - 2)
+      const second = new Date()
+      Vue.set(this.dates, 0, new Date(first))
+      Vue.set(this.dates, 1, new Date(second))
+    },
     previous() {
       if (this.dates.length) {
-        let range = (this.dates[1] - this.dates[0]) / (1000 * 60 * 60 * 24)
+        let range = (this.dates[1] - this.dates[0]) / (1000 * 60 * 60 * 24) + 1
         const first = this.dates[0].setDate(this.dates[0].getDate() - range)
         const second = this.dates[1].setDate(this.dates[1].getDate() - range)
         Vue.set(this.dates, 0, new Date(first))
@@ -57,11 +82,12 @@ export default {
 
     next() {
       if (this.dates.length) {
-        let range = (this.dates[1] - this.dates[0]) / (1000 * 60 * 60 * 24)
+        let range = (this.dates[1] - this.dates[0]) / (1000 * 60 * 60 * 24) + 1
         const first = this.dates[0].setDate(this.dates[0].getDate() + range)
         const second = this.dates[1].setDate(this.dates[1].getDate() + range)
         Vue.set(this.dates, 0, new Date(first))
         Vue.set(this.dates, 1, new Date(second))
+        console.log(range)
       }
     }
   }
@@ -70,7 +96,13 @@ export default {
 
 <style>
 .main {
-  margin: 1rem;
+  margin: 50px;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start !important;
 }
 
 .calendar > .label {
